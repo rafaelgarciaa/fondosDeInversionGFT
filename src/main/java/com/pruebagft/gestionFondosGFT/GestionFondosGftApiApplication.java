925289a1d9faa0509ddbcef472eee689003599d7
Rafael <rafangarac@gmail.com>
2025-07-14 15:24:22 -0500

    Initial Commit: SpringBoot Proyect with base dependencies

---
+++ src/main/java/com/pruebagft/gestionFondosGFT/GestionFondosGftApiApplication.java
@@ -0,0 +1,13 @@
+package com.pruebagft.gestionFondosGFT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFondosGftApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFondosGftApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebagft/gestionFondosGFT/GestionFondosGftApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebagft.gestionFondosGFT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFondosGftApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
